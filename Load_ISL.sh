09fd95ebacd5f623552280ce40c492abee989d1c
Jernestos <ernijost-erngihubst@yahoo.com>
2022-09-30 11:41:05 +0200

    Update Load_ISL.sh

--- Load_ISL.sh
+++ Load_ISL.sh
@@ -2,4 +2,8 @@
 echo "Start cloning"
 rm -rf Information-Security-Lab
 git clone https://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@github.com/Jernestos/Information-Security-Lab.git
+chmod 755 ./Information-Security-Lab/Load_ISL.sh
+mv ./Information-Security-Lab/Load_ISL.sh ~/Desktop
 echo "Cloning terminated"
+echo "Start test"
+python3.9 Information-Security-Lab/1/Week\ 3\ Lab\ files/module_1_ECDSA_Cryptanalysis_Skel.py
