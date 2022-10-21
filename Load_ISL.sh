f7e5e81ef26b7a30902001335fd568f23c29e872
Jernestos <ernijost-erngihubst@yahoo.com>
2022-10-21 21:15:50 +0200

    Update Load_ISL.sh

--- Load_ISL.sh
+++ Load_ISL.sh
@@ -3,4 +3,3 @@ echo "Start cloning"
 rm -rf Information-Security-Lab
 git clone https://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@github.com/Jernestos/Information-Security-Lab.git
 echo "Cloning terminated"
-echo "Start test"
