d0be9e2010e65fb3f785da4e1c1a247bd0af9bb2
Jernestos <ernijost-erngihubst@yahoo.com>
2022-11-06 21:07:37 +0100

    UPdate

--- Load_ISL.sh
+++ Load_ISL.sh
@@ -10,8 +10,8 @@ echo "Sync START"
 
 echo "Sync exe1"
 cd exercise1
-scp ./exploit1a.py isl-env:/home/student/handout/exercise1
-scp ./exploit1b.py isl-env:/home/student/handout/exercise1
+#scp ./exploit1a.py isl-env:/home/student/handout/exercise1
+#scp ./exploit1b.py isl-env:/home/student/handout/exercise1
 npx isl-tool@latest submit ex1a ./exploit1a.py
 npx isl-tool@latest submit ex1b ./exploit1b.py
 cd ..
@@ -19,8 +19,8 @@ echo "Sync exe1 - DONE"
 
 echo "Sync exe2"
 cd exercise2
-scp ./exploit2a.py isl-env:/home/student/handout/exercise2
-scp ./exploit2b.py isl-env:/home/student/handout/exercise2
+#scp ./exploit2a.py isl-env:/home/student/handout/exercise2
+#scp ./exploit2b.py isl-env:/home/student/handout/exercise2
 npx isl-tool@latest submit ex2a ./exploit2a.py
 npx isl-tool@latest submit ex2b ./exploit2b.py
 cd ..
@@ -28,8 +28,8 @@ echo "Sync exe2 - DONE"
 
 echo "Sync exe3"
 cd exercise3
-scp ./exploit3a.py isl-env:/home/student/handout/exercise3
-scp ./exploit3b.py isl-env:/home/student/handout/exercise3
+#scp ./exploit3a.py isl-env:/home/student/handout/exercise3
+#scp ./exploit3b.py isl-env:/home/student/handout/exercise3
 npx isl-tool@latest submit ex3a ./exploit3a.py
 npx isl-tool@latest submit ex3b ./exploit3b.py
 cd ..
@@ -37,8 +37,8 @@ echo "Sync exe3 - DONE"
 
 echo "Sync exe4"
 cd exercise4
-scp ./exploit4a.py isl-env:/home/student/handout/exercise4
-scp ./exploit4b.py isl-env:/home/student/handout/exercise4
+#scp ./exploit4a.py isl-env:/home/student/handout/exercise4
+#scp ./exploit4b.py isl-env:/home/student/handout/exercise4
 npx isl-tool@latest submit ex4a ./exploit4a.py
 npx isl-tool@latest submit ex4b ./exploit4b.py
 cd ..
@@ -46,8 +46,8 @@ echo "Sync exe4 - DONE"
 
 echo "Sync exe5"
 cd exercise5
-scp ./exploit5a.py isl-env:/home/student/handout/exercise5
-scp ./exploit5b.py isl-env:/home/student/handout/exercise5
+#scp ./exploit5a.py isl-env:/home/student/handout/exercise5
+#scp ./exploit5b.py isl-env:/home/student/handout/exercise5
 npx isl-tool@latest submit ex5a ./exploit5a.py
 npx isl-tool@latest submit ex5b ./exploit5b.py
 cd ..
@@ -55,8 +55,8 @@ echo "Sync exe5 - DONE"
 
 echo "Sync exe6"
 cd exercise6
-scp ./exploit6a.py isl-env:/home/student/handout/exercise6
-scp ./exploit6b.py isl-env:/home/student/handout/exercise6
+#scp ./exploit6a.py isl-env:/home/student/handout/exercise6
+#scp ./exploit6b.py isl-env:/home/student/handout/exercise6
 npx isl-tool@latest submit ex6a ./exploit6a.py
 npx isl-tool@latest submit ex6b ./exploit6b.py
 cd ..
