d5aab857fe0983c737c79553f95bf3f1a5573fd3
Jernestos <ernijost-erngihubst@yahoo.com>
2022-11-18 22:47:07 +0100

    c

---
+++ update_CTF.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Start cloning"
+#https://stackoverflow.com/questions/62177166/how-to-do-git-commit-using-personal-access-token
+git clone https://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@https://github.com/Jernestos/CTF.git
+echo "Cloning terminated"
+echo "Sync START"
